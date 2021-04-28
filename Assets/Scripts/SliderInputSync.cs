c0fa506eed10b7e04b76b60ce456d2a03f27097b
Mitya Shabat <mitya.shabat@gmail.com>
2021-04-28 18:57:56 +0300

    Create functionality for different kinds of menus

---
+++ Assets/Scripts/SliderInputSync.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderInputSync : MonoBehaviour
+{
+    private Slider slider;
+    private InputField input;
+    private void Start()
+    {
+        slider = GetComponentInChildren<Slider>();
+        input = GetComponentInChildren<InputField>();
+        input.text = slider.value.ToString();
+        slider.onValueChanged.AddListener(f => input.text = f.ToString());
+        input.onValueChanged.AddListener(s => {
+            int f;
+            if (s == "")
+            {
+                f = (int)slider.minValue;
+                slider.value = f;
+            }
+            else
+            {
+                f = int.Parse(s);
+                f = (int)Mathf.Clamp(f, slider.minValue, slider.maxValue);
+                input.text = f.ToString();
+                slider.value = f;
+            }
+        });
+    }
+}
---
+++ Assets/Scripts/WaveTypeMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveTypeMenuController : MonoBehaviour
+{
+    private void OnPingCounterIncrement(int vertex)
+    {
+        GetComponentInChildren<Counter>().Increment();
+    }
+    private void OnEnable()
+    {
+        Mesh polyMeshToSet = GetComponentInChildren<MeshSelector>().GetSelectedMesh();
+        GameObject.Find("Polyhedra").GetComponent<Polyhedra>().SetMesh(polyMeshToSet);
+
+        GameObject.Find("App").GetComponent<Simulation>().pingEvent.AddListener(OnPingCounterIncrement);
+    }
+    private void OnDisable()
+    {
+        GameObject.Find("App").GetComponent<Simulation>().pingEvent.RemoveListener(OnPingCounterIncrement);
+    }
+}
